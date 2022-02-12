8568b062869c9f9dbbd12f9038b29ecc064aa167
Renan Soares <renansoaresffc@gmail.com>
2022-02-12 20:28:15 -0300

    Responsive

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// Tooltip tecnologias
+tippy('.html5-icon', {
+    content: 'HTML 5',
+});
+
+tippy('.css3-icon', {
+    content: 'CSS 3',
+});
+
+tippy('.javascript-icon', {
+    content: 'Javascript',
+});
+
+tippy('.jquery-icon', {
+    content: 'jQuery',
+});
